1266aed21eb85da40e3f57a39193ca2698957704
Mszempruch <marcin.szempruch@gmail.com>
2022-09-08 14:49:57 +0200

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Infrastructure;
+using ToDoList.Models;
+
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext context;
+        public ToDoController(ToDoContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+        }
+
+        // GET /todo/create
+        public IActionResult Create() => View();
+
+        // POST /todo/crete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Sukces"] = "Zadanie zostało dodane do listy!";
+
+                return RedirectToAction("Index");
+            }
+            
+
+            return View(item);
+        }
+        // GET /todo/edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound(item);
+            }
+
+            return View(item);
+        }
+        // POST /todo/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Sukces"] = "Zadanie zostało zaktualizowane!";
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View(item);
+        }
+        // GET /todo/delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                TempData["Błąd"] = "Zadanie nie istnieje!";
+            }
+            else
+            {
+                context.ToDoList.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Sukces"] = "Zadanie zostało usunięte!";
+
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
